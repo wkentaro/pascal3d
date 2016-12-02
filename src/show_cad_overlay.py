715585f1f94cd23311161488a0dcd0ec05120d84
Kentaro Wada <www.kentaro.wada@gmail.com>
2016-12-02 23:06:51 +0900

    Stop skipping val:0

--- src/show_cad_overlay.py
+++ src/show_cad_overlay.py
@@ -7,12 +7,8 @@ def main():
     data_type = 'val'
     dataset = pascal3d.dataset.Pascal3DDataset(data_type)
     for i in xrange(len(dataset)):
-        if data_type == 'val' and i == 0:
-            print('Skipping invalid data: id={}, data_type={}'
-                  .format(i, data_type))
-            continue
-        print('Showing cad overlay: id={}, data_type={}'
-              .format(i, data_type))
+        print('[{dtype}:{id}] showing cad overlay'
+              .format(dtype=data_type, id=i))
         dataset.show_cad_overlay(i)
 
 
