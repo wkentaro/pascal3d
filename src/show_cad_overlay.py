02005325611cf7c8d8424d49d27135827d8187fe
Kentaro Wada <www.kentaro.wada@gmail.com>
2016-12-02 22:13:58 +0900

    Print message while showing cad overlay

--- src/show_cad_overlay.py
+++ src/show_cad_overlay.py
@@ -4,8 +4,15 @@ import pascal3d
 
 
 def main():
-    dataset = pascal3d.dataset.Pascal3DDataset('val')
+    data_type = 'val'
+    dataset = pascal3d.dataset.Pascal3DDataset(data_type)
     for i in xrange(len(dataset)):
+        if data_type == 'val' and i == 0:
+            print('Skipping invalid data: id={}, data_type={}'
+                  .format(i, data_type))
+            continue
+        print('Showing cad overlay: id={}, data_type={}'
+              .format(i, data_type))
         dataset.show_cad_overlay(i)
 
 
