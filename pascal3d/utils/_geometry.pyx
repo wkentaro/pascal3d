afb190e7e6c383a72aea8b3a44898578b2eb08c5
Kentaro Wada <www.kentaro.wada@gmail.com>
2016-12-07 05:53:17 +0900

    Fix for multiple triangles of intersect3d_ray_triangle

--- pascal3d/utils/_geometry.pyx
+++ pascal3d/utils/_geometry.pyx
@@ -39,38 +39,38 @@ def intersect3d_ray_triangle(
 
     ray_dir = ray1 - ray0
     w0 = ray0 - tri0
-    a = - np.dot(n, w0.T)
-    b = np.dot(n, ray_dir.T)
-    invalid = (np.abs(b) < 1e-6)[:, 0]
-    invalid_2 = np.bitwise_and(invalid, (a == 0)[:, 0])
+    a = - (n * w0).sum(axis=-1)
+    b = (n * ray_dir).sum(axis=-1)
+    invalid = np.abs(b) < 1e-6
+    invalid_2 = np.bitwise_and(invalid, a == 0)
     flags[invalid_2] = 2
     intersection[invalid_2, ...] = np.nan
-    invalid_0 = np.bitwise_and(invalid, (a != 0)[:, 0])
+    invalid_0 = np.bitwise_and(invalid, a != 0)
     flags[invalid_0] = 0
     intersection[invalid_0, ...] = np.nan
 
     r = a / b
-    invalid = (r < 0)[:, 0]
+    invalid = r < 0
     flags[invalid] = 0
     intersection[invalid, ...] = np.nan
 
     # compute intersection point
-    I = ray0 + r * ray_dir
+    I = ray0 + r[:, np.newaxis].repeat(3, axis=-1) * ray_dir
 
     # compute inside/outside of plane
-    uu = np.dot(u, u.T)
-    uv = np.dot(u, v.T)
-    vv = np.dot(v, v.T)
+    uu = (u * u).sum(axis=-1)
+    uv = (u * v).sum(axis=-1)
+    vv = (v * v).sum(axis=-1)
     w = I - tri0
-    wu = np.dot(w, u.T)
-    wv = np.dot(w, v.T)
+    wu = (w * u).sum(axis=-1)
+    wv = (w * v).sum(axis=-1)
     D = uv * uv - uu * vv
     s = (uv * wv - vv * wu) / D
-    invalid = np.bitwise_or((s < 0)[:, 0], (s > 1)[:, 0])
+    invalid = np.bitwise_or(s < 0, s > 1)
     flags[invalid] = 0
     intersection[invalid] = I[invalid]
     t = (uv * wu - uu * wv) / D
-    invalid = np.bitwise_or((t < 0)[:, 0], (t > 1)[:, 0])
+    invalid = np.bitwise_or(t < 0, t > 1)
     flags[invalid] = 0
     intersection[invalid] = I[invalid]
 
@@ -101,13 +101,19 @@ def raytrace_camera_frame_on_triangles(
     cdef unsigned int n_triangles = len(pts_tri0)
     cdef np.ndarray[DTYPE_t, ndim=1] pt_camera_frame
     cdef np.ndarray[DTYPE_t, ndim=2] intersects
-    cdef np.ndarray[DTYPE_t, ndim=2] depth = np.zeros((n_points,), dtype=DTYPE)
+    cdef np.ndarray[DTYPE_t, ndim=1] depth = np.zeros((n_points,), dtype=DTYPE)
+    cdef DTYPE_t d
     for i_pt in range(n_points):
+        print(100.0 * i_pt / n_points)
         pt_camera_frame = pts_camera_frame[i_pt]
         ray0 = np.repeat(pt_camera_origin[np.newaxis, :], n_triangles, axis=0)
         ray1 = np.repeat(pt_camera_frame[np.newaxis, :], n_triangles, axis=0)
         flags, intersects = intersect3d_ray_triangle(
             ray0, ray1, pts_tri0, pts_tri1, pts_tri2)
-        d = np.abs(intersects[flags == 1]).min()
+        intersects = intersects[flags == 1]
+        if intersects.size == 0:
+            d = np.nan
+        else:
+            d = np.abs(intersects).min()
         depth[i_pt] = d
     return depth
