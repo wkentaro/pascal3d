c6355a27cbe36f02847c293e5653c93dc12cf4a4
Kentaro Wada <www.kentaro.wada@gmail.com>
2016-12-07 03:36:51 +0900

    Add example of raytracing for 3d ray and triangle

--- examples/utils/geometry/intersect3d_ray_triangle.py
+++ examples/utils/geometry/intersect3d_ray_triangle.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+import time
+
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -8,27 +10,42 @@ import pascal3d
 
 def main():
     ray0 = np.array([[0., 0., 0.]])
-    ray1 = np.array([[-0.2, 0.2, -1.]])
+    ray1 = np.array([[-0.2, -0.2, -1.]])
 
     tri0 = np.array([[1., 0., -2.]])
-    tri1 = np.array([[-1., -1., -2.]])
-    tri2 = np.array([[-1., 1., -2.]])
+    Tri1 = np.array([
+        [-1., -1., -2.],
+        [-1., -1., -0.5],
+        [-1., -1., 1.],
+    ])
+    Tri2 = np.array([
+        [-1., 1., -2.],
+        [-1., 1., -2.],
+        [-1., 1., -2.],
+    ])
+
+    Ray0 = np.repeat(ray0, 3, axis=0)
+    Ray1 = np.repeat(ray1, 3, axis=0)
+    Tri0 = np.repeat(tri0, 3, axis=0)
 
     ax = plt.figure().gca(projection='3d')
 
+    t_start = time.time()
     flags, intersects = pascal3d.utils.intersect3d_ray_triangle(
-        ray0, ray1, tri0, tri1, tri2)
-    assert flags[0] == 1
-    intersect = intersects[0]
+        Ray0, Ray1, Tri0, Tri1, Tri2)
+    print('elapsed_time: {:.3} [s]'.format(time.time() - t_start))
+    intersects = intersects[flags == 1]
 
-    x, y, z = zip(*np.vstack((ray0, ray1, intersect)))
-    ax.plot(x, y, z, color='k', marker='o')
-    x, y, z = zip(*ray0)
-    ax.plot(x, y, z, color='r', marker='o', markersize=5)
-    x, y, z = zip(*intersect[np.newaxis, :])
-    ax.plot(x, y, z, color='g', marker='o', markersize=5)
-    x, y, z = zip(*np.vstack((tri0, tri1, tri2, tri0)))
+    x, y, z = zip(*np.vstack((ray0, ray1, intersects)))
     ax.plot(x, y, z, color='b', marker='o')
+    x, y, z = zip(*ray0)
+    ax.plot(x, y, z, color='r', marker='o', markersize=10)
+    x, y, z = zip(*intersects)
+    ax.plot(x, y, z, color='g', marker='o', markersize=10)
+
+    for c, tri0, tri1, tri2 in zip('ymc', Tri0, Tri1, Tri2):
+        x, y, z = zip(*np.vstack((tri0, tri1, tri2, tri0)))
+        ax.plot(x, y, z, color=c, marker='o')
 
     plt.show()
 
