e4a58d4d797bb8189a998ef3060d3d2404008057
Kentaro Wada <www.kentaro.wada@gmail.com>
2016-12-15 22:31:50 +0900

    Place pcd file for testing

--- tests/utils_tests/test_io.py
+++ tests/utils_tests/test_io.py
@@ -1,22 +1,18 @@
-import tempfile
+import os.path as osp
 
 import nose.tools
 import numpy as np
-import requests
+
+
+this_dir = osp.dirname(osp.realpath(__file__))
 
 
 def test_load_pcd():
     from pascal3d.utils.io import load_pcd
 
-    pcd_file = 'http://raw.githubusercontent.com/PointCloudLibrary/pcl/pcl-1.8.0/test/bunny.pcd'  # NOQA
-    res = requests.get(pcd_file)
-    assert res.status_code == 200
+    pcd_file = osp.join(this_dir, 'data/bunny.pcd')
 
-    tmp_file = tempfile.mktemp() + '.pcd'
-    with open(tmp_file, 'w') as f:
-        f.write(res.content)
-
-    points = load_pcd(tmp_file)
+    points = load_pcd(pcd_file)
     nose.tools.assert_equal(points.shape, (397, 3))
     nose.tools.assert_equal(points.dtype, np.float64)
     np.testing.assert_array_almost_equal(
