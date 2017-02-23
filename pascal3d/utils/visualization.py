f8442449b9f45e6687c983a14410de1faadc716e
Kentaro Wada <www.kentaro.wada@gmail.com>
2017-02-24 08:04:21 +0900

    Use nan_mask when visualizing

--- pascal3d/utils/visualization.py
+++ pascal3d/utils/visualization.py
@@ -11,6 +11,7 @@ def colorize_depth(depth, min_value, max_value):
         print('WARNING: max_value is inf.', file=sys.stderr)
     colorized = depth.copy()
     nan_mask = np.isnan(colorized)
+    colorized[nan_mask] = 0
     colorized = 1. * (colorized - min_value) / (max_value - min_value)
     colorized = matplotlib.cm.jet(colorized)[:, :, :3]
     colorized = (colorized * 255).astype(np.uint8)
